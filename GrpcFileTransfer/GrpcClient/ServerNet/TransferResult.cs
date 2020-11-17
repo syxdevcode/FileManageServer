3d75fefe276c4bf7a326ff36d4adaaafcd949e22
1981371953@qq.com <1981371953@qq.com>
2020-11-17 13:54:30 +0800

    grpc services

---
+++ GrpcFileTransfer/GrpcClient/ServerNet/TransferResult.cs
@@ -0,0 +1,24 @@
+﻿namespace GrpcClient.ServerNet
+{
+    /// <summary>
+    /// 传输结果
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    internal class TransferResult<T>
+    {
+        /// <summary>
+        /// 传输是否成功
+        /// </summary>
+        public bool IsSuccessful { get; set; }
+
+        /// <summary>
+        /// 消息
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// 标记类型
+        /// </summary>
+        public T Tag { get; set; } = default;
+    }
+}
---
+++ GrpcFileTransfer/GrpcServer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Grpc.Core;
+
+namespace GrpcServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //提供服务
+            Server server = new Server()
+            {
+                Services = { GrpcService.FileTransfer.BindService(new FileImpl()) },
+                Ports = { new ServerPort("127.0.0.1", 50000, ServerCredentials.Insecure) }
+            };
+            //服务开始
+            server.Start();
+
+            while (string.Compare(Console.ReadLine()?.Trim(), "exit", StringComparison.OrdinalIgnoreCase) == 0)
+            {
+
+            }
+            //结束服务
+            server.ShutdownAsync();
+        }
+    }
+}
